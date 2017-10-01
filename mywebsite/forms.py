e49a524719232d3d0614340de784d888452cd188
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2017-10-01 23:30:59 +0530

    feat: Basic Image,html and css file changed

---
+++ mywebsite/forms.py
@@ -0,0 +1,34 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+# If you don't do this you cannot use Bootstrap CSS
+
+class LoginForm(forms.Form):
+    username = forms.CharField(label="Username", max_length=30,
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'name': 'username'}))
+    password = forms.CharField(label="Password", max_length=30,
+                               widget=forms.PasswordInput(attrs={'class': 'form-control', 'name': 'password'}))
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+
+    birth_date = forms.DateField(help_text='Required. Format: YYYY-MM-DD')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'birth_date', 'email', 'password1', 'password2',)
+
+    def clean_email(self):
+        if User.objects.filter(email=self.cleaned_data.get('email', None)).count() > 0:
+            raise forms.ValidationError("User with this email already exists")
+
+        return self.cleaned_data.get('email')
+
+    def clean_username(self):
+        if User.objects.filter(username=self.cleaned_data.get('username', None)).count() > 0:
+            raise forms.ValidationError("User with this username already exists")
+
+        return self.cleaned_data.get('username')
