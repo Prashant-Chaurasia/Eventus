567bf18599da759df62ce976fa36c0043da0506a
pillu <pilluverma@gmail.com>
2017-12-04 12:51:43 +0530

    added contactus, updateprofile, suggestevent, circle_button

--- Eventus/keys.py
+++ Eventus/keys.py
@@ -1,7 +1,7 @@
-DB_NAME = 'dcr5c76v4475k3'
-DB_PASS = '4eb06b3049881ad5315ef6712b21bb075de927c5f59ef8237686354902376e90'
-DB_USER = 'sllmghuxtpmmke'
-DB_HOST = 'ec2-184-72-223-199.compute-1.amazonaws.com'
+DB_NAME = 'eventus'
+DB_PASS = 'eventus'
+DB_USER = 'eventus'
+DB_HOST = 'localhost'
 DB_PORT = '5432'
-ALLOWED_HOST = 'event-us.herokuapp.com'
-DEBUG_STATUS = False
+ALLOWED_HOST = '*'
+DEBUG_STATUS = True
